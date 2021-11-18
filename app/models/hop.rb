775cf59c9532d9272d1c265a4f5e687efa97567a
ivan-direct <natedipiazza@gmail.com>
2021-11-18 16:07:24 -0700

    add new_varieties scope

--- app/models/hop.rb
+++ app/models/hop.rb
@@ -4,6 +4,8 @@ class Hop < ApplicationRecord
   has_and_belongs_to_many :beers
 
   validates :name, presence: true
+  # Warning: this will return everything until the system matures.
+  scope :new_varieties, -> { where('created_at >= ?', Date.today - 6.months).order('created_at desc').limit(10) }
 
   def total_checkins
     return 0 if beers.blank?
@@ -13,25 +15,26 @@ class Hop < ApplicationRecord
 
   def calculate_ranking
     # this will be the calculation for a hops rating #
-    rankings = []
-    ## TODO Build hop list. Iterate through #
-    Hop.all each do |hop|
-      # TODO: hit Untappd API using custom query
-      # response = GetBeers(hop.name) ####################################
-      response = OpenStruct.new(name: "TODO") # ERASE ME
-      # TODO: find/create beer record
-      Beer.find_or_create_by(name: response.name)
-      # TODO: calc rating
-      rating = beers.map(&:rating).sum / beers.size
-      rankings << OpenStruct.new(hash_id: hop.id, rating: rating)
-    end
-    # TODO: sort ratings to get ranking
-    rankings.sort_by!(&rating)
-    # TODO: and finally, loop through array using the index to insert rankings for each hop
-    rankings.each_index do |i|
-      ranking = rankings[i]
-      hop = Hop.find(ranking.hop_id)
-      hop.update(ranking: i + 1) # top rank is one
-    end
+    # rankings = []
+    # # TODO Build hop list. Iterate through #
+    # Hop.all each do |hop|
+    #   # TODO: hit Untappd API using custom query
+    #   # response = API::GetBeers(hop.name)
+    #   beers = response.beers.map do |res_beer|
+    #     # TODO: find/create beer record
+    #     Beer.find_or_create_by(name: res_beer.name)
+    #   end
+    #   # TODO: calc rating
+    #   rating = beers.map(&:rating).sum / beers.size
+    #   rankings << OpenStruct.new(hash_id: hop.id, rating: rating)
+    # end
+    # # TODO: sort ratings to get ranking
+    # rankings.sort_by!(&rating)
+    # # TODO: and finally, loop through array using the index to insert rankings for each hop
+    # rankings.each_index do |i|
+    #   ranking = rankings[i]
+    #   hop = Hop.find(ranking.hop_id)
+    #   hop.update(ranking: i + 1) # top rank is one
+    # end
   end
 end
--- spec/models/hop_spec.rb
+++ spec/models/hop_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Hop, type: :model do
   let(:juicy_bits) { create(:beer, name: 'Juicy Bits', checkins: 53_700) }
   let(:citra_xx_juicy_bits) { create(:beer, name: 'Citra Extra Extra Juicy Bits', checkins: 2_100) }
 
-  describe 'create' do
+  describe '#create' do
     it 'creates a new hop' do
       expect(create(:hop, name: 'Citra')).to be_truthy
     end
@@ -22,14 +22,32 @@ RSpec.describe Hop, type: :model do
     end
   end
 
-  describe 'beers' do
+  context 'scopes' do
+    describe 'new_varieties' do
+      let(:chinook) { create(:hop, name: 'Chinook', created_at: Date.today - 7.months) }
+
+      before do
+        11.times do |i|
+          create(:hop, name: "X-9N0#{i}", created_at: Date.today - i.days)
+        end
+      end
+      it 'returns the 10 most recent' do
+        hops = Hop.new_varieties
+        expect(hops.size).to eq(10)
+        expect(hops.include?(chinook)).to be_falsey
+        expect(hops.first.name).to eq('X-9N00') # created today should be first
+      end
+    end
+  end
+
+  describe '#beers' do
     it 'adds beer associations to a hop' do
       hop.beers << [juicy_bits, citra_xx_juicy_bits]
       expect(hop.beers.size).to eq(2)
     end
   end
 
-  describe 'total_checkins' do
+  describe '#total_checkins' do
     let(:expected_checkins) { juicy_bits.checkins + citra_xx_juicy_bits.checkins }
 
     it 'returns the sum of all beer checkins' do
