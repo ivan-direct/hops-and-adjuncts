2f3cc6c2d25741348800902a4bae9de7513df69c
ivan-direct <natedipiazza@gmail.com>
2021-11-20 16:01:37 -0700

    kludge to get inner link to work in jest test

--- app/javascript/__tests__/Hops.test.js
+++ app/javascript/__tests__/Hops.test.js
@@ -5,6 +5,7 @@ import { render, waitFor, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Hops from "../components/Hops";
 import "./Hops.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const server = setupServer(
   rest.get("http://localhost/api/v1/hops", (req, res, ctx) => {
@@ -22,7 +23,13 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 test("loads and displays greeting", async () => {
-  render(<Hops />);
+  render(
+    <Router>
+      <div>
+        <Hops />
+      </div>
+    </Router>
+  );
 
   await waitFor(() => screen.getByText("Mosaic"));
   expect(screen.getByText("Citra")).toBeInTheDocument();
