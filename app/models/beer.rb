14708b333441b9203b72a3c25e5b19b199a06c6f
ivan-direct <natedipiazza@gmail.com>
2021-12-01 22:11:05 -0700

    aired out app/models

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# the origin of hop data using Untappd (or other) data feed as the source for all hop stats.
 class Beer < ApplicationRecord
   has_and_belongs_to_many :hops
   belongs_to :brewery, optional: true
@@ -9,4 +10,12 @@ class Beer < ApplicationRecord
   validates :checkins, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than: 5.01 }
   validates :style, inclusion: { in: %w[ipa stout other] }
+
+  def self.calculate_rating(hop)
+    beers = select {|beer| beer.hops.include? hop}
+    rating = beers.map(&:rating).sum / beers.size
+    hop.update(rating: rating)
+    OpenStruct.new(hop_id: hop.id, rating: hop.rating)
+  end
+
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Beer supplemental class: Brewery Name, City, and State
 class Brewery < ApplicationRecord
   has_many :beers, dependent: :nullify
 
