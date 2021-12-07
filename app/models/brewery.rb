68b277b0dd9d4579fafe77834bb761117b15cf5c
ivan-direct <natedipiazza@gmail.com>
2021-12-07 00:43:47 -0700

    unit tests for Brewery / finder modules

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -12,7 +12,7 @@ class Brewery < ApplicationRecord
   def self.populate_external_codes
     where(external_code: nil).find_each do |brewery|
       brewery.populate_external_code
-      sleep 5 # to avoid hammering their server
+      sleep 5 if Rails.env != 'test' # to avoid hammering their server
     end
   end
 
@@ -20,14 +20,14 @@ class Brewery < ApplicationRecord
   def self.build_beer_list
     where.not(external_code: nil).find_each do |brewery|
       brewery.find_beers
-      sleep 5 # to avoid hammering their server
+      sleep 5 if Rails.env != 'test' # to avoid hammering their server
     end
   end
 
   # using Brewery#name query Untappd for exact match # # # # #
   # if found it updates the record # # # # # # # # # # # # # #
   def populate_external_code
-    url = URI.parse("https://untappd.com/search?q=#{name}&type=brewery")
+    url = "https://untappd.com/search?q=#{name}&type=brewery"
     BreweryFinder.build(url, self)
   end
 
@@ -36,7 +36,7 @@ class Brewery < ApplicationRecord
   def find_beers
     BeerFinder::TYPE_IDS.each do |type_id|
       url = "https://untappd.com/#{external_code}/beer?type_id=#{type_id}&sort=created_at_desc"
-      BeerFinder.build(url, self)
+      BeerFinder.build(url, id)
     end
   end
 end
