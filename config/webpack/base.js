0113ddfb6175846f65c180684a763c914e9a7bf6
ivan-direct <natedipiazza@gmail.com>
2021-12-07 17:31:25 -0700

    still can't get antd styling to load.

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,3 +1,29 @@
-const { webpackConfig } = require('@rails/webpacker')
+const { merge } = require('webpack-merge');
+const { webpackConfig } = require("@rails/webpacker");
 
-module.exports = webpackConfig
+loaderConfig = {
+  entry: "./app/javascript/packs/application.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: ["style-loader", "less-loader"],
+      },
+    ],
+  },
+};
+
+console.log(webpackConfig);
+
+module.exports = merge(loaderConfig, webpackConfig);
