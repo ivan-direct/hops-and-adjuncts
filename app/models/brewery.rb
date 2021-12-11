e1fe32a86a916a7b7d4939c3a07a48f4998114d4
ivan-direct <natedipiazza@gmail.com>
2021-12-11 11:46:54 -0700

    add some checks to limit request pace

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -17,14 +17,16 @@ class Brewery < ApplicationRecord
   end
 
   # calls #find_beers for all records containing an external_code
-  def self.build_beer_list
+  def self.build_beer_list(style)
+    raise 'unknown style' unless %w[ipa stout].include?(style)
+
     where.not(external_code: nil).find_each do |brewery|
-      brewery.find_beers
+      brewery.find_beers(style)
       sleep 5 unless Rails.env.test? # to avoid hammering their server
     end
   end
 
-  def self.mark_as_invalid id
+  def self.mark_as_invalid(id)
     brewery = where(id: id)
     brewery.update(code_invalid: true) if brewery.present?
   end
@@ -38,11 +40,12 @@ class Brewery < ApplicationRecord
 
   # Find the 25 most recent beers for stouts and ipas (other type maybe) # #
   # and create a beer record if a hop name is found in the description # # #
-  def find_beers
-    type_ids =  BeerFinder::IPA_TYPE_IDS+BeerFinder::STOUT_TYPE_IDS
+  def find_beers(style)
+    type_ids = style == 'ipa' ? BeerFinder::IPA_TYPE_IDS : BeerFinder::STOUT_TYPE_IDS
     type_ids.each do |type_id|
       url = "https://untappd.com/#{external_code}/beer?type_id=#{type_id}&sort=created_at_desc"
       BeerFinder.build(url, id)
+      sleep 10 unless Rails.env.test? 
     end
   end
 end
